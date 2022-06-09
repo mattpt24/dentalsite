a411aab7103690c78d4085892962895caeacc65c
unknown <mattypt@hotmail.co.uk>
2022-06-09 13:32:10 +0200

    Made more changes

--- src/Reviews.js
+++ src/Reviews.js
@@ -3,42 +3,72 @@ export const Reviews = [
         quote: "I've found the reception staff very helpful and they've always been able to book me within a reasonable time.",
         name: "Chris Jameson",
         age: "52",
-        src: "https://cdn.pixabay.com/photo/2018/08/18/18/50/sunset-3615276_1280.jpg"
+        src: "http://res.cloudinary.com/ulaw/image/upload/c_fit,w_500/dryer_randy_2018_sx8btg.jpg"
     },
 
     {
         quote: "I’m so impressed with the results that I’m now going to book in every 3 months.",
         name: "Sharon Daniels",
-        age: "44",
-        src: "https://cdn.pixabay.com/photo/2018/08/18/18/50/sunset-3615276_1280.jpg"
+        age: "32",
+        src: "https://i.pinimg.com/originals/de/64/80/de64801f0275c1ab2ea5a9e2bb3ce7bc.jpg"
     },
 
     {
         quote: "Couldn't recommend their services enough, so professional and friendly!",
         name: "Keith Thompson",
         age: "64",
-        src: "https://cdn.pixabay.com/photo/2018/08/18/18/50/sunset-3615276_1280.jpg"
+        src: "https://scontent-lhr8-1.xx.fbcdn.net/v/t1.6435-9/74912315_10212149281366832_6356507840953188352_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=174925&_nc_ohc=HsvQuqENXWMAX8NRCjl&_nc_ht=scontent-lhr8-1.xx&oh=00_AT9GtD6-wzh1myxGYj6RFFYnnXhnR1xuYnZfh1HlAwXT6Q&oe=62C765A5"
     },
 
     {
         quote: "I have been to see Denpure 4 times in 2 months and they are amazing!",
         name: "Aleem Chatur",
-        age: "31",
-        src: "https://cdn.pixabay.com/photo/2018/08/18/18/50/sunset-3615276_1280.jpg"
+        age: 45,
+        src: "https://im.rediff.com/news/2015/mar/08prabha-kumar.jpg"
     },
 
     {
         quote: "This has got to be the friendliest dental practice in London. The actual treatment was very professional!",
         name: "Edward Baah",
-        age: 51,
-        src: "https://cdn.pixabay.com/photo/2018/08/18/18/50/sunset-3615276_1280.jpg"
+        age: 35,
+        src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiwCcJFOxnIkVVK0i9I8P7dck7sJXFiklDpg&usqp=CAU"
     },
 
     {
         quote: "Would recommend this practice and dentist to anyone",
         name: "Lauren Wyatt",
-        age: 24,
-        src: "https://cdn.pixabay.com/photo/2018/08/18/18/50/sunset-3615276_1280.jpg"
+        age: 29,
+        src: "https://randomuser.me/api/portraits/women/33.jpg"
+    },
+
+    {
+        quote: "I really enjoyed my visit, it was very professional and all of the staff were so nice!",
+        name: "Jane Armstrong",
+        age: 27,
+        src: "https://randomuser.me/api/portraits/women/92.jpg"
+    },
+
+
+    {
+        quote: "If you're in need of dental work , Denpure is the obvious choice for me. Amazing work",
+        name: "Monica Arnold",
+        age: 18,
+        src: "https://randomuser.me/api/portraits/women/86.jpg"
+    },
+
+
+    {
+        quote: "The go to place for dental work! So happy with the work they have done on my teeth",
+        name: "Dana Prescott",
+        age: 32,
+        src: "https://randomuser.me/api/portraits/women/68.jpg"
+    },
+
+    {
+        quote: "My Family has been visiting Denpure for many years where we receive second to none services at all times",
+        name: "Tommy Silva",
+        age: 25,
+        src: "https://randomuser.me/api/portraits/men/98.jpg"
     }
 ]
 
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,67 +3,45 @@ import "../scss/components/sections/testimonials.scss";
 import TestCard from "./TestCard";
 import {Reviews} from "../Reviews";
 
+
 function Testimonials() {
-    return (
-        <div className="testimonial_main_container">
-            <div className="testimonial_padding_container">  
-            <p className="section_sml_title">TESTIMONIALS</p>
-            <h1 className="section_title services_title">Hear what people have to say.</h1>
 
-            <div className="testimonial_card_section">
+const toggleButton = document.querySelector(".read-more");
+const moreCards = document.querySelector(".extend-cards");
 
-                <TestCard 
-                quote={Reviews[0].quote}
-                name={Reviews[0].name}
-                age={Reviews[0].age}
-                src={Reviews[0].src}
-                />
+function showMoreCards() {
+    moreCards.classList.toggle("more-cards");
+}
 
-                <TestCard 
-                quote={Reviews[1].quote}
-                name={Reviews[1].name}
-                age={Reviews[1].age}
-                src={Reviews[1].src}
-                />
+return (
+    <section id="testimonials">
+<div className="testimonial_main_container">
+    <div className="testimonial_padding_container">
+        <p className="section_sml_title">TESTIMONIALS</p>
+        <h1 className="section_title services_title">Hear what people have to say.</h1>
 
-
-               <TestCard 
-                quote={Reviews[2].quote}
-                name={Reviews[2].name}
-                age={Reviews[2].age}
-                src={Reviews[2].src}
-                />
-
-                <TestCard 
-                quote={Reviews[3].quote}
-                name={Reviews[3].name}
-                age={Reviews[3].age}
-                src={Reviews[3].src}
-                />
-
-                <TestCard 
-                quote={Reviews[4].quote}
-                name={Reviews[4].name}
-                age={Reviews[4].age}
-                src={Reviews[4].src}
-                />
-
-
-                <TestCard 
-                quote={Reviews[5].quote}
-                name={Reviews[5].name}
-                age={Reviews[5].age}
-                src={Reviews[5].src}
-                />
-
-            </div>
-
-            <center>
-            <a className="read-more btn btn-white" target="_blank" href="https://www.google.com/search?q=google+reviews+denpure&rlz=1C1CHBF_en-GBGB973GB973&oq=google+reviews+denpure&aqs=chrome..69i57j69i64.4006j0j7&sourceid=chrome&ie=UTF-8#lrd=0x48766c481668bccd:0x3b1762b7f7b150bd,1,,,">Read More</a>
-            </center>
-            </div>
+        <div className="testimonial_card_section">
+            <TestCard quote={Reviews[0].quote} name={Reviews[0].name} age={Reviews[0].age} src={Reviews[0].src} />
+            <TestCard quote={Reviews[1].quote} name={Reviews[1].name} age={Reviews[1].age} src={Reviews[1].src} />
+            <TestCard quote={Reviews[2].quote} name={Reviews[2].name} age={Reviews[2].age} src={Reviews[2].src} />
         </div>
-    )
+
+        <div className="testimonial_card_section extend-cards">
+            <TestCard quote={Reviews[3].quote} name={Reviews[3].name} age={Reviews[3].age} src={Reviews[3].src} />
+            <TestCard quote={Reviews[4].quote} name={Reviews[4].name} age={Reviews[4].age} src={Reviews[4].src} />
+            <TestCard quote={Reviews[5].quote} name={Reviews[5].name} age={Reviews[5].age} src={Reviews[5].src} />
+            <TestCard quote={Reviews[6].quote} name={Reviews[6].name} age={Reviews[6].age} src={Reviews[6].src} />
+            <TestCard quote={Reviews[7].quote} name={Reviews[7].name} age={Reviews[7].age} src={Reviews[7].src} />
+            <TestCard quote={Reviews[8].quote} name={Reviews[8].name} age={Reviews[8].age} src={Reviews[8].src} />
+        </div>
+    </div>
+    <div class="btn_container">
+        <a onClick={showMoreCards} className="read-more btn btn-white" target="_blank">Read More</a>
+        <a  className="read-more btn btn-transparent" target="_blank" href="https://www.google.com/search?q=depnpure&rlz=1C1CHBF_en-GBGB973GB973&oq=depnpure&aqs=chrome..69i57j46i13j0i13j46i13l2j0i13j46i13j0i13j0i13i457.3520j0j9&sourceid=chrome&ie=UTF-8#lrd=0x48766c481668bccd:0x3b1762b7f7b150bd,1,,,">View All</a>
+        </div>
+</div>
+</section>
+)
 }
 
 export default Testimonials;
